3dc93c819a60510bf5ee2fdb39b5d6a3069ba669
kachup1 <emailkarlahere@gmail.com>
2025-05-19 13:16:49 -0700

    added analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { inject } from '@vercel/analytics';
+
+inject();
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
