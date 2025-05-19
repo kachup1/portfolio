53990045c2281b76993d1c096f4245140bbe61cf
kachup1 <emailkarlahere@gmail.com>
2025-05-18 20:42:05 -0700

    recaptcha centering

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -114,18 +114,15 @@ function Contact() {
                     onChange={(e) => setBotField(e.target.value)}
                     style={{ display: 'none' }}
                 />
-
+                
+                <div className="recaptcha-wrapper">
+                <ReCAPTCHA
+                    sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+                    onChange={(token) => setCaptchaToken(token)}
+                />
+                </div>
 
                 <div className="button-container">
-
-                <div style={{ display: 'flex', justifyContent: 'center', marginTop: '1rem', marginBottom: '2rem' }}>
-                    <div style={{ display: 'inline-block' }}>
-                        <ReCAPTCHA
-                        sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
-                        onChange={(token) => setCaptchaToken(token)}
-                        />
-                    </div>
-                </div>
 
                     <button type="button" className="submit-button" onClick={handleSubmit}>Submit</button>
                     {statusMessage && (
